79f3e414d51357a3b64ee3da137b86655f3f6f07
ElenaNProkhorova <167714416+ElenaNProkhorova@users.noreply.github.com>
2024-05-27 12:07:56 +0300

    Add files via upload
    первый коммит

---
+++ qa_java_copy/src/test/java/TestExceptionLion.java
@@ -0,0 +1,29 @@
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class TestExceptionLion {
+    String sex;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {"unknown"},
+                {null},
+                {"Не самец"},
+                {"Не самка"}
+        };
+    }
+
+    public TestExceptionLion(String sex) {
+        this.sex = sex;
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionLionTest() throws Exception {
+            new Lion(sex, null);
+    }
+}
---
+++ qa_java_copy/src/test/java/TestFeline.java
@@ -0,0 +1,37 @@
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestFeline {
+    @Spy
+    Feline feline;
+
+    @Test
+    public void felineGetKittensNoParam () {
+        feline.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void felineGetKittens () {
+        when(feline.getKittens(1)).thenReturn(1);
+    }
+
+    @Test
+    public void felineGetFamily () {
+        when(feline.getFamily()).thenReturn("Кошачьи");
+    }
+
+    @Test
+    public void felineEatMeat () throws Exception {
+        feline.eatMeat();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+}
